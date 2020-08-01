87ca74800195868f731403c589d1e6337591f857
Marcelo Jr <ec2-user@ip-172-31-46-82.sa-east-1.compute.internal>
2020-08-01 01:53:01 +0000

    Aula 3

---
+++ src/components/FormField/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function FormField({ label, type, value, name, onChange }) {
+    if (type === "textarea")
+        return (
+            <div>
+                <label>
+                    {label}: 
+                    <textarea
+                        value={value}
+                        name={name}
+                        onChange={onChange}
+                    />
+                </label>
+            </div>
+    )
+    else
+        return (
+            <div>
+                <label>
+                    {label}: 
+                    <input
+                        type={type}
+                        value={value}
+                        name={name}
+                        onChange={onChange}
+                    />
+                </label>
+            </div>
+    )
+}
+
+export default FormField;
--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,24 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+    const [categorias, setCategorias] = useState([]);
+    const [novaCategoria, setNovaCategoria] = useState({
+        nome: '',
+        descricao: '',
+        cor: ''
+    });
+    
+    function setValue(key, value){
+        setNovaCategoria({
+            ...novaCategoria,
+            [key]: value,
+        });
+    }
+    
+    function handleChange(e) {
+        setValue(e.target.getAttribute('name'), e.target.value);
+    }
+    
     return (
         <PageDefault>
             <h1>Cadastro de Categoria</h1>
                 
-            <form>
-                <label>
-                    Nome da Categoria:
-                    <input
-                        type="text"
-                    />
-                </label>
-                
+            <form onSubmit={e => {
+                    e.preventDefault();
+                    setCategorias([
+                        ...categorias,
+                        novaCategoria
+                    ]);
+                }
+            }>
+            
+                <FormField
+                    label="Nome"
+                    type="text"
+                    name="nome"
+                    value={novaCategoria.nome}
+                    onChange={handleChange}
+                />
+                <FormField
+                    label="Descricao"
+                    type="textarea"
+                    name="descricao"
+                    value={novaCategoria.descricao}
+                    onChange={handleChange}
+                />
+                <FormField
+                    label="Cor"
+                    type="color"
+                    name="cor"
+                    value={novaCategoria.cor}
+                    onChange={handleChange}
+                />
                 <button>
                     Cadastrar
                 </button>
             </form>
+            
+            <br/>
+            
+            <ul>
+                {categorias.map(categoria => <li key={categoria.nome}>{categoria.nome}</li>)}
+            </ul>
             
             <br/>
             
